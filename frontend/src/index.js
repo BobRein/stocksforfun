b425d85e6d56ecfec85b81e1aadd33ab3b874808
BobRein <governorrein@gmail.com>
2019-12-21 11:10:51 -0600

    Added axios interceptor and hash router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import axios from 'axios';
+import { HashRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//these cause, error, why were they in the journalism project???? 
 
+// axios.defaults.headers.post['Content-Type'] = 'application/json';
+// axios.defaults.headers.put['Content-Type'] = 'application/json';
+
+// axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
+
+// not sure if I need this 
+axios.interceptors.request.use(request => {	
+	console.log(request);
+
+	return request;
+}, error => Promise.reject(error));
+
+ReactDOM.render(
+	<HashRouter>
+    	<App />
+  	</HashRouter>
+	, document.getElementById('root')); 
 
