8bfd5726a2a26f65677d25704f783de164d3df28
BobRein <governorrein@gmail.com>
2019-12-22 15:02:39 -0600

    Added results to lookup and also error checking

--- frontend/src/axios/stockCalls.js
+++ frontend/src/axios/stockCalls.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
-export function getTicker(ticker){
+export function getPrice(ticker){
     return axios.get(`https://financialmodelingprep.com/api/v3/stock/real-time-price/`+ticker)
+}
+export function getStockInfo(ticker){
+    return axios.get(`https://financialmodelingprep.com/api/v3/company/profile/`+ticker)
 }
--- frontend/src/components/lookup.js
+++ frontend/src/components/lookup.js
@@ -1,16 +1,39 @@
 import {TextField, Typography } from '@material-ui/core';
 import React from 'react';
-import { getTicker } from '../axios/stockCalls.js';
+import { getStockInfo } from '../axios/stockCalls.js';
 
 
 class Lookup extends React.Component {
-    update = (event) => {
+    state = {}
+    constructor(props) {
+        super(props);
+        this.state = {
+            stockError: '',
+            timeout: null
+        }
+    }
+    handleStockChange = (event) => {
         var ticker = event.target.value;
-        getTicker(ticker).then((response) => {
-            console.log(response.data.symbol + ': ' + response.data.price);
-        }).catch((error) => {
-            console.log('Stock error for ',ticker,' ',error.response);
-        });
+        clearTimeout(this.state.timeout);
+            this.state.timeout = setTimeout(() => {
+            getStockInfo(ticker).then((response) => {
+                if (response.data.symbol == undefined){
+                    this.setState({ stockError: ticker.toUpperCase() + ' is not available.' });
+                    this.setState({ name: '' });
+                    this.setState({ price: '' });
+                }else{
+                    console.log(response.data.symbol + ': ' + response.data.price);
+                    this.setState({ name: response.data.profile.companyName });
+                    this.setState({ price: response.data.profile.price });
+                    this.setState({ stockError: '' });
+                    
+                }
+            }).catch((error) => {
+                console.log('Stock error for ',ticker,' ',error.response);
+                this.setState({ stockError: 'Server issue, sorry for the inconvience.' })
+            });
+        }, 1000);
+        
     }
     render() {
         let style = {
@@ -19,9 +42,17 @@ class Lookup extends React.Component {
         }
         return (
             <div style={{width: '100%', padding: '10px'}}>
-                <TextField id="standard-basic" label="Ticker Lookup" onChange ={(e) => this.update(e)}/>
-                
+                <TextField 
+                id="standard-basic"
+                label="Ticker Lookup"
+                error={this.state.stockError != ''}
+                helperText ={this.state.stockError}
+                onChange ={(e) => this.handleStockChange(e)}/>
+           
+            <div >
+            {this.state.name} $ {this.state.price}
             </div>
+        </div>
         );
     }
 }
